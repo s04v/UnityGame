7a098772fab791d8c66ff8927d0b3250b279f7cb
Vitaliy <43200383+s04v@users.noreply.github.com>
2021-01-24 18:10:09 +0100

    map

---
+++ SurvivalGame/Assets/Scripts/Enemy/Spawner.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject enemy;
+    float randX;
+    Vector2 wheteToSpawn;
+    public float spawnRate = 2f;
+    float nextSpawn = 0.0f;
+
+    int StartEnemyNumber = 2;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if(Time.time > nextSpawn)
+        {
+            nextSpawn = Time.time + spawnRate;
+            for (int i = 0; i < 2; i++)
+            {
+                randX = Random.Range(-1f, 1f);
+                wheteToSpawn = new Vector2(transform.position.x, transform.position.y);
+                Instantiate(enemy, wheteToSpawn, Quaternion.identity);
+               // StartEnemyNumber += 2;
+             }
+        }
+
+    }
+}
